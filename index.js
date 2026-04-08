3a749c6a796ef427f66519236c0ef141fee7463a
hajime.takeuchi <shadowthe8156@gmail.com>
2026-04-08 23:31:40 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+// 文字列型の変数
+const str = "Hello World2222222!!";
+console.log(str);
+
+// 数字型の変数
+const num = 1234;
+console.log(num);
+
+// 真偽値型の変数
+const bool = false;
+console.log(bool);
+
+// 文字列型の連結
+const hoge = "ほげ";
+const fuga= "ふが";
+console.log(hoge + fuga);
+
+// 文字列型の連結2(よく使う)
+const sei = "竹内";
+const mei = "甫";
+const aisatsu = `おかえりなさい！ ${sei} ${mei} さん`;
+console.log(aisatsu);
